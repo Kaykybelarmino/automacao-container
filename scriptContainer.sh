7508d3e323795b150375b90c0e35f5e6f834f229
Kayky <kayky.pedroso@sptech.school>
2023-11-02 10:42:48 -0300

    feat: adição de comandos para aumentar a segurança do código e app

--- scriptContainer.sh
+++ scriptContainer.sh
@@ -2,10 +2,13 @@
 
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo docker pull mysql
 sudo docker volume create volume-mysql
 sudo docker image build -t mysql-image -f mysql.dockerfile . 
 sudo docker run -d -p 3306:3306 --name container-mysql -v "volume-mysql:/var/lib/mysql" mysql-image 
+sudo docker start container-mysql
 sudo docker image build -t java-image -f java.dockerfile .
 sudo docker run --name -p 90:90 container-java java-image 
 
