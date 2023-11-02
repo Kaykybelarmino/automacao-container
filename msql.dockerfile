5b018e45e5c7b761dae2d36f21216686301bd226
Kayky <kayky.pedroso@sptech.school>
2023-11-02 10:04:18 -0300

    feat: adição de mysql.dockerfile para solucionar os comandos MySQL

---
+++ msql.dockerfile
@@ -0,0 +1,7 @@
+FROM mysql:8.0
+ENV MYSQL_ROOT_PASSWORD=segredo
+ENV MYSQL_DATABASE=marvel
+ENV MYSQL_USER=medconnect
+ENV MYSQL_PASSWORD=medconnect123
+
+COPY script_formatadoV1.sql /docker-entrypoint-initdb.d/
--- scriptContainer.sh
+++ scriptContainer.sh
@@ -3,9 +3,10 @@
 sudo apt update -y && sudo apt upgrade -y
 sudo apt install docker.io -y
 sudo docker pull mysql
-sudo docker volume create volume-mysql 
-sudo docker run -d -p 3306:3306 --name container-mysql -v "volume-mysql:/var/lib/mysql" -e "MYSQL_ROOT_PASSWORD=segredo" mysql 
-sudo docker cp script_formatadoV1.sql "container-mysql:script_formatadoV1.sql"
-sudo docker exec container-mysql mysql -u root -psegredo -e "CREATE USER 'medconnect'@'%'IDENTIFIED BY 'medconnect123';" && sudo docker exec container-mysql mysql -u root -psegredo -e "GRANT ALL PRIVILEGES ON *.* TO 'medconnect'@'%' WITH GRANT OPTION;" && sudo docker exec container-mysql mysql -u root -psegredo < /script_formatadoV1.sql
+sudo docker volume create volume-mysql
+sudo docker image build -t mysql-image -f mysql.dockerfile . 
+sudo docker run -d -p 3306:3306 --name container-mysql -v "volume-mysql:/var/lib/mysql" --network host mysql-image 
 sudo docker image build -t java-image -f java.dockerfile .
-sudo docker run --name container-java java-image 
+sudo docker run --name container-java --network host java-image 
+
+
