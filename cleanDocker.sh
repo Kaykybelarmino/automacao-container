6d71dad1cd92d660bf5e3113fb71fcc3dc34c3a5
Kayky <kayky.pedroso@sptech.school>
2023-11-02 12:37:59 -0300

    fix: clean docker melhorado e java.dockerfile modificado

--- cleanDocker.sh
+++ cleanDocker.sh
@@ -3,6 +3,8 @@ sudo docker rm container-mysql
 sudo docker image rm mysql-image 
 sudo docker image rm mysql 
 sudo docker volume rm volume-mysql 
+sudo docker stop container-java
+sudo docker rm container-java
 cd ..
 rm -rf automacao-container 
 
--- java.dockerfile
+++ java.dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:19
 WORKDIR /usr/src/app/java
-COPY apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar .
+COPY app.jar .
