f451ddf5127f4b4916e09d805799891f9c1cb3fe
Kayky <kayky.pedroso@sptech.school>
2023-11-01 23:43:15 -0300

    fix: continuando corringdo conexão

--- scriptContainer.sh
+++ scriptContainer.sh
@@ -6,7 +6,7 @@ sudo docker pull mysql
 sudo docker volume create volume-mysql 
 sudo docker run -d -p 3306:3306 --name container-mysql -v "volume-mysql:/var/lib/mysql" -e "MYSQL_ROOT_PASSWORD=segredo" mysql 
 sudo docker cp script_formatadoV1.sql "container-mysql:script_formatadoV1.sql"
-sudo docker exec -it container-mysql mysql bash 
+sudo docker exec -it container-mysql bash 
 mysql -u root -psegredo -e "CREATE USER 'medconnect'@'%' IDENTIFIED BY 'medconnect123';"
 mysql -u root -psegredo -e "GRANT ALL PRIVILEGES ON *.* TO 'medconnect'@'%' WITH GRANT OPTION;"
 mysql -u root -psegredo < script_formatadoV1.sql
