5c2525498235050017ef58352ad8961f4f8d50ac
Kayky <kayky.pedroso@sptech.school>
2023-11-01 23:05:22 -0300

    fix: container sem nome e jar faltando

--- scriptContainer.sh
+++ scriptContainer.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt update -y && sudo apt upgrade
-sudo apt install docker.io
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install docker.io -y
 sudo docker pull mysql
 sudo docker volume create volume-mysql 
-sudo docker run -d -p 3306:3306 -w "volume-mysql:/var/lib/mysql" -e "MYSQL_ROOT_PASSWORD=segredo" 
+sudo docker run -d -p 3306:3306 --name container-mysql -w "volume-mysql:/var/lib/mysql" -e "MYSQL_ROOT_PASSWORD=segredo" 
 sudo docker cp script_formatadoV1.sql "container-mysql:script_formatadoV1.sql"
 sudo docker exec -it container-mysql mysql -u root -psegredo -e "CREATE USER 'medconnect'@'%' IDENTIFIED BY 'medconnect123'; GRANT ALL PRIVILEGES ON *.* TO 'medconnect'@'%' WITH GRANT OPTION; source script_formatadoV1.sql;" 
 sudo docker image build -t java-image -f java.dockerfile .
