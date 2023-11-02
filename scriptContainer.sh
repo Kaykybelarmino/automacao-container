ca9d66cbc0343fb4bcc6b93684a9410737544ab7
Kayky <kayky.pedroso@sptech.school>
2023-11-01 23:38:08 -0300

    fix: corrigindo manipulação no banco

--- scriptContainer.sh
+++ scriptContainer.sh
@@ -6,6 +6,10 @@ sudo docker pull mysql
 sudo docker volume create volume-mysql 
 sudo docker run -d -p 3306:3306 --name container-mysql -v "volume-mysql:/var/lib/mysql" -e "MYSQL_ROOT_PASSWORD=segredo" mysql 
 sudo docker cp script_formatadoV1.sql "container-mysql:script_formatadoV1.sql"
-sudo docker exec -it container-mysql mysql -u root -psegredo -e "CREATE USER 'medconnect'@'%' IDENTIFIED BY 'medconnect123'; GRANT ALL PRIVILEGES ON *.* TO 'medconnect'@'%' WITH GRANT OPTION; source script_formatadoV1.sql;" 
+sudo docker exec -it container-mysql mysql bash 
+mysql -u root -psegredo -e "CREATE USER 'medconnect'@'%' IDENTIFIED BY 'medconnect123';"
+mysql -u root -psegredo -e "GRANT ALL PRIVILEGES ON *.* TO 'medconnect'@'%' WITH GRANT OPTION;"
+mysql -u root -psegredo < script_formatadoV1.sql
+exit
 sudo docker image build -t java-image -f java.dockerfile .
 sudo docker run --name container-java java-image 
