934ce18b6797d2be8c31f0ccecb1c16d432e2410
Kayky <kayky.pedroso@sptech.school>
2023-11-02 11:30:30 -0300

    fix: ajustando script.sql

--- script_formatadoV1.sql
+++ script_formatadoV1.sql
@@ -3,6 +3,9 @@ DROP DATABASE IF EXISTS medconnect;
 CREATE DATABASE IF NOT EXISTS medconnect;
 USE medconnect;
 
+GRANT ALL PRIVILEGES ON *.* TO 'medconnect'@'%';
+FLUSH PRIVILEGES;
+
 CREATE TABLE IF NOT EXISTS Hospital (
     idHospital INT PRIMARY KEY AUTO_INCREMENT,
     nomeFantasia VARCHAR(45) NOT NULL,
@@ -102,7 +105,7 @@ CREATE TABLE IF NOT EXISTS SalaCirurgiao (
     fkHospitalSala INT,
     fkRoboSala INT,
     PRIMARY KEY (idSala, fkHospitalSala, fkRoboSala),
-    CONSTRAINT fkHospitalSala FOREIGN KEY (fkHospitalSala) REFERENCES hospital (idHospital),
+    CONSTRAINT fkHospitalSala FOREIGN KEY (fkHospitalSala) REFERENCES Hospital (idHospital),
     CONSTRAINT fkRoboSala FOREIGN KEY (fkRoboSala) REFERENCES robocirurgiao (idRobo)
 );
 
@@ -261,5 +264,76 @@ CREATE TABLE IF NOT EXISTS quantidadeAlerta (
     dtHora DATETIME
 );
 
-GRANT ALL PRIVILEGES ON *.* TO 'medconnect'@'%';
-FLUSH PRIVILEGES;
+DELIMITER $$
+CREATE TRIGGER criarAlerta
+AFTER INSERT ON Registros
+FOR EACH ROW
+BEGIN
+    DECLARE id_metrica INT;
+    DECLARE v_alerta DOUBLE;
+    DECLARE v_urgente DOUBLE;
+    DECLARE v_critico DOUBLE;
+    DECLARE v_componente VARCHAR(45);
+
+    SELECT fkMetrica, nome INTO id_metrica, v_componente
+    FROM componentes
+    WHERE NEW.fkComponente = idComponentes;
+
+    SELECT critico, urgente, alerta INTO v_critico, v_urgente, v_alerta
+    FROM Metrica
+    WHERE idMetrica = id_metrica;
+
+    IF NEW.dado >= v_critico THEN
+        INSERT INTO Alerta (tipo_alerta, fkRegistro, fkRobo, dtHora, nome_componente, dado)
+        VALUES ('critico', NEW.idRegistro, NEW.fkRoboRegistro, NOW(), v_componente, NEW.dado);
+    ELSEIF NEW.dado >= v_urgente THEN
+        INSERT INTO Alerta (tipo_alerta, fkRegistro, fkRobo, dtHora, nome_componente, dado)
+        VALUES ('urgente', NEW.idRegistro, NEW.fkRoboRegistro, NOW(), v_componente, NEW.dado);
+    ELSEIF NEW.dado >= v_alerta THEN
+        INSERT INTO Alerta (tipo_alerta, fkRegistro, fkRobo, dtHora, nome_componente, dado)
+        VALUES ('alerta', NEW.idRegistro, NEW.fkRoboRegistro, NOW(), v_componente, NEW.dado);
+    END IF;
+END;
+$$ DELIMITER ;
+
+-- Crie o procedimento inserir_qtd_alerta
+DELIMITER $$
+CREATE PROCEDURE inserir_qtd_alerta()
+BEGIN
+DECLARE qtdAlertaAlerta INT;
+DECLARE qtdAlertaUrgente INT;
+DECLARE qtdAlertaCritico INT;
+    
+SELECT COUNT(idAlerta) FROM Alerta
+WHERE tipo_alerta = "alerta" 
+AND dtHora <= date_sub(now(), INTERVAL 1 MINUTE)
+INTO qtdAlertaAlerta;
+    
+SELECT COUNT(idAlerta) FROM Alerta
+WHERE tipo_alerta = "urgente" 
+AND dtHora <= date_sub(now(), INTERVAL 1 MINUTE)
+INTO qtdAlertaUrgente;
+    
+SELECT COUNT(idAlerta) FROM Alerta
+WHERE tipo_alerta = "critico" 
+AND dtHora <= date_sub(now(), INTERVAL 1 MINUTE)
+INTO qtdAlertaCritico;
+    
+IF qtdAlertaAlerta > 15 THEN
+INSERT INTO quantidadeAlerta (tipo_alerta, dtHora)
+VALUES ("alerta", now());
+END IF;
+    
+IF qtdAlertaUrgente > 15 THEN
+INSERT INTO quantidadeAlerta (tipo_alerta, dtHora) 
+VALUES ("urgente", now());
+END IF;
+    
+IF qtdAlertaCritico > 15 THEN
+INSERT INTO quantidadeAlerta (tipo_alerta, dtHora) 
+VALUES ("critico", now());
+END IF;
+    
+END;
+ $$ DELIMITER ;
+
