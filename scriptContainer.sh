76f74eadc46bdc29acc9370395386f7e39e7bc3c
Kayky <kayky.pedroso@sptech.school>
2023-11-02 23:19:59 -0300

    fix: modificações no endereçamento de IP

--- scriptContainer.sh
+++ scriptContainer.sh
@@ -8,11 +8,11 @@ sudo docker volume create volume-mysql
 sudo docker image build -t mysql-image -f mysql.dockerfile . 
 sudo docker run -d -p 3306:3306 --name container-mysql -v "volume-mysql:/var/lib/mysql" mysql-image
 sudo docker network create --subnet=192.168.0.0/24 conexao-mysql
-sudo docker network connect --ip=192.168.0.0/24 conexao-mysql container-mysql
+sudo docker network connect --ip=192.168.0.1/24 conexao-mysql container-mysql
 sudo docker start container-mysql
 sudo docker image build -t java-image -f java.dockerfile .
 sudo docker run -d --name container-java java-image 
-sudo docker network connect --ip=192.168.0.0/24 conexao-mysql container-java 
+sudo docker network connect --ip=192.168.0.2/24 conexao-mysql container-java 
 sudo docker start container-java
 sudo docker exec -i container-java java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
 
