bbf1ebf7bcdef6a71f8917dd900f63c015527841
Kayky <kayky.pedroso@sptech.school>
2023-11-04 11:53:57 -0300

    fix: correção dos erros de sintaxe do script

--- scriptSolucao.sh
+++ scriptSolucao.sh
@@ -4,16 +4,16 @@ sudo apt update -y && sudo apt upgrade -y
 
 java --version
 
-if [ $? != 0 ] then;
+if [ $? -ne 0 ]; then
     echo "Inicializando instalação do java"
-    sudo apt install openjdk-20-jre-headless
+    sudo apt install openjdk-19-jre-headless -y
 fi
 
 docker --version
 
-if [ $? != 0 ] then;
+if [ $? -ne 0 ]; then
     echo "Inicializando instalação do docker"
-    sudo apt install docker.io
+    sudo apt install docker.io -y
 fi
 
 echo "Inicializando o serviço docker"
@@ -23,22 +23,20 @@ sudo systemctl enable docker
 echo "Criando volume do MySQL"
 sudo docker volume create volume-mysql
 
-echo "Criando imagem MySQL"
-
-sudo docker image build -t mysql-image -f mysql.dockerfile . 
+echo "Construindo imagem MySQL"
+sudo docker build -t mysql-image -f mysql.dockerfile .
 
 echo "Criando container MySQL"
-sudo docker run -d -p 3306:3306 --name container-mysql -v "volume-mysql:/var/lib/mysql" mysql-image
+sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/var/lib/mysql mysql-image
 
-echo "Construindo imagem python"
-sudo docker image build -t python-image -f python.dockerfile .
+echo "Construindo imagem Python"
+sudo docker build -t python-image -f python.dockerfile .
 
 echo "Criando container Python"
 sudo docker run -d -p 80:80 --name container-python --network host python-image
 
 echo "Instalando automação via macro"
-sudo apt-get install xdotool
-
+sudo apt-get install xdotool -y
 
 echo "Executando nossa solução.."
 sudo java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
@@ -51,3 +49,5 @@ sudo xdotool key crtl+q
 echo "Entrando no banco de dados.."
 sudo docker exec -it container-mysql bash
 mysql -u medconnect -p medconnect123
+
+
