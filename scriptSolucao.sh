a9b8112fe67820dfdc0918de716ffd9ef4747208
Kayky <kayky.pedroso@sptech.school>
2023-11-07 18:59:44 -0300

    test: modificando scriptSolucao

--- scriptSolucao.sh
+++ scriptSolucao.sh
@@ -29,22 +29,7 @@ sudo docker image build -t mysql-image -f mysql.dockerfile .
 echo "Criando container MySQL"
 sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/var/lib/mysql mysql-image 
 
-echo "Instalando automação via macro"
-sudo apt-get install xdotool -y
-
-sudo docker stop container-mysql
 sudo docker start container-mysql
 sleep 3
 
 sudo java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
-
-echo "Entrando no banco de dados.."
-sudo docker exec -it container-mysql bash
-mysql -u medconnect -p medconnect123
-
-
-# echo "Construindo imagem Python"
-# sudo docker build -t python-image -f python.dockerfile .
-
-# echo "Criando container Python"
-# sudo docker run -d -p 80:80 --name container-python --network host python-image
