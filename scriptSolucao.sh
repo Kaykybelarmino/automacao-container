a8341cab055936ce9e0bda6c06f07ba2e1262f79
Kayky <kayky.pedroso@sptech.school>
2023-11-07 20:09:37 -0300

    fix: script de extensão produz nenhum efeito, retornando a versão original dos script

--- scriptSolucao.sh
+++ scriptSolucao.sh
@@ -4,14 +4,14 @@ sudo apt update -y && sudo apt upgrade -y
 
 java --version
 
-if [ $? -ne 0 ]; then
+if [ $? -gt 0 ]; then
     echo "Inicializando instalação do java"
     sudo apt install openjdk-19-jre-headless -y
 fi
 
 docker --version
 
-if [ $? -ne 0 ]; then
+if [ $? -gt 0 ]; then
     echo "Inicializando instalação do docker"
     sudo apt install docker.io -y
 fi
@@ -29,6 +29,7 @@ sudo docker image build -t mysql-image -f mysql.dockerfile .
 echo "Criando container MySQL"
 sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/var/lib/mysql mysql-image
 
-. scriptExtensao
+sudo docker start container-mysql
+sudo java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 
