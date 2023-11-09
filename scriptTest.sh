2ee5b2ab9c193edf4d1d02f6b93590ec1704e572
Kayky <kayky.pedroso@sptech.school>
2023-11-09 19:23:13 -0300

    fix: corrigindo comandos desnecessários

--- scriptTest.sh
+++ scriptTest.sh
@@ -25,9 +25,6 @@ sudo docker volume create volume-mysql
 
 echo "Criando container MySQL"
 sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/var/lib/mysql -e "MYSQL_ROOT_PASSWORD=segredo"  -e "MYSQL_INITDB_SKIP_TZINFO=yes" -e "MYSQL_DATABASE=medconnect" mysql:8.0 --init-file=script_formatadoV1.sql
-sudo docker cp script_formatadoV1.sql "container-mysql:script_formatadoV1.sql"
-sudo docker start container-mysql
-sudo docker exec -i container-mysql sh -c 'mysql -u root -psegredo < script_formatadoV1.sql'
 sudo java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 
