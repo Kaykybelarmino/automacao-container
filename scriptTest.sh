8e4b7a19d0797ea5cc68b0b098d44c98045ef091
Kayky <kayky.pedroso@sptech.school>
2023-11-09 18:42:26 -0300

    test: testando novo jar e novo script com variáveis de ambiente direto no comando

---
+++ scriptTest.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo apt update -y && sudo apt upgrade -y
+
+java --version
+
+if [ $? -ne 0 ]; then
+    echo "Inicializando instalação do java"
+    sudo apt install openjdk-19-jre-headless -y
+fi
+
+docker --version
+
+if [ $? -ne 0 ]; then
+    echo "Inicializando instalação do docker"
+    sudo apt install docker.io -y
+fi
+
+echo "Inicializando o serviço docker"
+sudo systemctl start docker
+sudo systemctl enable docker
+
+echo "Criando volume do MySQL"
+sudo docker volume create volume-mysql
+
+echo "Criando container MySQL"
+sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/var/lib/mysql -e "MYSQL_ROOT_PASSWORD=segredo" -e "MYSQL_DATABASE=medconnect" mysql:8.0
+sudo docker cp script_formatadoV1.sql "container-mysql:script_formatadoV1.sql"
+sudo docker exec container-mysql -e "mysql -u root -psegredo" -e "source script_formatadoV1.sql"
+sudo docker start container-mysql
+sudo java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+
