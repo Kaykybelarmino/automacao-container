cced5a9f3722c1758cfdf1ca3bc134f531aa686d
Kayky <kayky.pedroso@sptech.school>
2023-11-09 21:19:12 -0300

    fix: erro de sintaxe

--- scriptTest.sh
+++ scriptTest.sh
@@ -25,7 +25,7 @@ sudo docker volume create volume-mysql
 
 echo "Criando container MySQL" 
 sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/docker-entrypoint-initdb.d -e "MYSQL_ROOT_PASSWORD=segredo" -e "MYSQL_DATABASE=medconnect" -e "MYSQL_INITDB_SKIP_TZINFO=yes" mysql:8.0
-sudo docker script_formatadoV1.sql container-mysql:/docker-entrypoint-initdb.d/
+sudo docker cp script_formatadoV1.sql container-mysql:/docker-entrypoint-initdb.d/
 sudo docker stop container-mysql
 sudo docker start container-mysql
 sudo docker ps -a
