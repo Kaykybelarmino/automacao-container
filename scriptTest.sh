59289c18feab0a5845954ef8a1a185ef679c0d9c
Kayky <kayky.pedroso@sptech.school>
2023-11-09 21:17:41 -0300

    test: tentativa simplificada de executar o código

--- scriptTest.sh
+++ scriptTest.sh
@@ -22,10 +22,12 @@ sudo systemctl enable docker
 
 echo "Criando volume do MySQL"
 sudo docker volume create volume-mysql
-sudo docker run --rm -v volume-mysql:/docker-entrypoint-initdb.d/ -w /docker-entrypoint-initdb.d/ busybox cp /home/ubuntu/automacao-container/script_formatadoV1.sql .
 
 echo "Criando container MySQL" 
 sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/docker-entrypoint-initdb.d -e "MYSQL_ROOT_PASSWORD=segredo" -e "MYSQL_DATABASE=medconnect" -e "MYSQL_INITDB_SKIP_TZINFO=yes" mysql:8.0
+sudo docker script_formatadoV1.sql container-mysql:/docker-entrypoint-initdb.d/
+sudo docker stop container-mysql
+sudo docker start container-mysql
 sudo docker ps -a
 sudo java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
 
