5f6d5e771c5303135c5cbea3f0ed51ce0dddad75
Kayky <kayky.pedroso@sptech.school>
2023-11-26 09:59:32 -0300

    fix: script de automação funcionando!

--- scriptTest.sh
+++ scriptTest.sh
@@ -23,17 +23,18 @@ sudo systemctl enable docker
 echo "Criando volume do MySQL"
 sudo docker volume create volume-mysql
 
-echo "Criando container MySQL" 
-sudo docker image build -t mysql-image -f mysql.dockerfile . 
+echo "Criando container MySQL"
+sudo docker image build -t mysql-image -f mysql.dockerfile .
 sudo docker run -d -p 3306:3306 --name container-mysql -v volume-mysql:/docker-entrypoint-initdb.d -e "MYSQL_ROOT_PASSWORD=segredo" -e "MYSQL_DATABASE=medconnect" -e "MYSQL_INITDB_SKIP_TZINFO=yes" mysql-image
 
 # Aguarda até que o MySQL esteja pronto para aceitar conexões
-while ! docker exec container-mysql mysqladmin ping --silent; do
-    sleep 1
-done
+#while ! docker exec container-mysql mysqladmin ping --silent; do
+sleep 15
+#done
 
 sudo docker ps -a
 java -jar apiLoocaTeste1-1.0-SNAPSHOT-jar-with-dependencies.jar
+
 
 
 
